f48f412c0b8903ee7008a5e01313dfd2d1ac67dc
Developer1 <developer1@BunthoeunMacMini.local>
2022-03-05 16:45:13 +0700

    update UI

--- employees/leave-history.php
+++ employees/leave-history.php
@@ -39,6 +39,8 @@
     <link rel="stylesheet" href="../assets/css/responsive.css">
     <!-- modernizr css -->
     <script src="../assets/js/vendor/modernizr-2.8.3.min.js"></script>
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../assets/css/adminlte.min.css">
 </head>
 
 <body>
@@ -47,180 +49,182 @@
         <div class="loader"></div>
     </div>
     <!-- preloader area end -->
-    <!-- page container area start -->
-    <div class="page-container">
-        <!-- sidebar menu area start -->
-        <div class="sidebar-menu">
-            <div class="sidebar-header">
-                <div class="logo">
-                    <a href="leave.php"><img src="../assets/images/icon/eleave-logo.png" alt="logo"></a>
-                </div>
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fa fa-bars"></i></a>
+            </li>
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+            <!-- Notifications Dropdown Menu -->
+            <!-- Notification bell -->
+            <?php include '../includes/admin-notification.php'?>
+
+            <li class="nav-item">
+                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="ti-fullscreen"></i>
+                </a>
+            </li>
+            
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+         <!-- Main Sidebar Container -->
+         <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link">
+            <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">Eleave</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column metismenu" id="menu">
+
+                        <li class="#">
+                            <a href="leave.php" aria-expanded="true"><i class="ti-user"></i><span>Apply Leave
+                                </span></a>
+                        </li>
+
+                        <li class="active">
+                            <a href="leave-history.php" aria-expanded="true"><i class="ti-agenda"></i><span>View My Leave History
+                                </span></a>
+                        </li>
+
+                    </ul>
+                </nav>
             </div>
-            <div class="main-menu">
-                <div class="menu-inner">
-                    <nav>
-                        <ul class="metismenu" id="menu">
+            <!-- /.sidebar -->
+        </aside>
+        <!-- Main Sidebar Container end-->
 
-                            <li class="#">
-                                <a href="leave.php" aria-expanded="true"><i class="ti-user"></i><span>Apply Leave
-                                    </span></a>
-                            </li>
-
-                            <li class="active">
-                                <a href="leave-history.php" aria-expanded="true"><i class="ti-agenda"></i><span>View My Leave History
-                                    </span></a>
-                            </li>
-
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-        <!-- sidebar menu area end -->
-        <!-- main content area start -->
-        <div class="main-content">
-            <!-- header area start -->
-            <div class="header-area">
-                <div class="row align-items-center">
-                    <!-- nav and search button -->
-                    <div class="col-md-6 col-sm-8 clearfix">
-                        <div class="nav-btn pull-left">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                        
-                    </div>
-                    <!-- profile info & task notification -->
-                    <div class="col-md-6 col-sm-4 clearfix">
-                        <ul class="notification-area pull-right">
-                            <li id="full-view"><i class="ti-fullscreen"></i></li>
-                            <li id="full-view-exit"><i class="ti-zoom-out"></i></li>
-                            
-                            
-                            
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <!-- header area end -->
-            <!-- page title area start -->
-            <div class="page-title-area">
-                <div class="row align-items-center">
-                    <div class="col-sm-6">
-                        <div class="breadcrumbs-area clearfix">
-                            <h4 class="page-title pull-left">My Leave History</h4>  
-                        </div>
-                    </div>
-                    <div class="col-sm-6 clearfix">
-                         <?php include '../includes/employee-profile-section.php'?>
-                    </div>
-                </div>
-            </div>
-            <!-- page title area end -->
-            <div class="main-content-inner">
-                <div class="row">
-                    <!-- data table start -->
-                    <div class="col-12 mt-5">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="header-title">Leave History Table</h4>
-                                <?php if($error){?><div class="alert alert-danger alert-dismissible fade show"><strong>Info: </strong><?php echo htmlentities($error); ?>
-                                <button type="button"  class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            
-                             </div><?php } 
-                                 else if($msg){?><div class="alert alert-success alert-dismissible fade show"><strong>Info: </strong><?php echo htmlentities($msg); ?> 
-                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                 </div><?php }?>
-                                <div class="data-tables">
-                                    <table id="dataTable" class="table table-hover progress-table text-center">
-                                        <thead class="bg-light text-capitalize">
-                                            <tr>
-                                                <th>#</th>
-                                                <th width="150">Type</th>
-                                                <th>Conditions</th>
-                                                <th>From</th>
-                                                <th>To</th>
-                                                <th width="150">Applied</th>
-                                                <th width="120">Admin's Remark</th>
-                                                <th>Status</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php 
-                                        $eid=$_SESSION['eid'];
-                                        $sql = "SELECT LeaveType,ToDate,FromDate,Description,PostingDate,AdminRemarkDate,AdminRemark,Status from tblleaves where empid=:eid";
-                                        $query = $dbh -> prepare($sql);
-                                        $query->bindParam(':eid',$eid,PDO::PARAM_STR);
-                                        $query->execute();
-                                        $results=$query->fetchAll(PDO::FETCH_OBJ);
-                                        $cnt=1;
-                                        if($query->rowCount() > 0){
-                                        foreach($results as $result)
-                                        {  ?> 
-
-                                            <tr>
-                                            <td> <?php echo htmlentities($cnt);?></td>
-                                            <td><?php echo htmlentities($result->LeaveType);?></td>
-                                            <td><?php echo htmlentities($result->Description);?></td>
-                                            <td><?php echo htmlentities($result->FromDate);?></td>
-                                            <td><?php echo htmlentities($result->ToDate);?></td>
-                                            <td><?php echo htmlentities($result->PostingDate);?></td>
-                                            <td><?php if($result->AdminRemark=="")
-                                            {
-                                            echo htmlentities('Pending');
-                                            } else {
-
-                                            echo htmlentities(($result->AdminRemark)." "."at"." ".$result->AdminRemarkDate);
-                                            }
-
-                                            ?>
-                                            </td>
-
-                                            <td> <?php $stats=$result->Status;
-                                                if($stats==1){
-                                             ?>
-                                                 <span style="color: green">Approved</span>
-                                                 <?php } if($stats==2)  { ?>
-
-                                                <span style="color: red">Not Approved</span>
-                                                 <?php } if($stats==0)  { ?>
-
-                                                <span style="color: blue">Pending</span>
-                                                <?php } ?>
-
-                                             </td>
-                                        </tr>
-
-                                         <?php $cnt++;} }?>
-                                          
-                                        </tbody>
-                                    </table>
+        <div class="content-wrapper">
+            <!-- page container area start -->
+            <div class="page-container pl-0">
+                <!-- main content area start -->
+                <div class="main-content">
+                    <!-- page title area start -->
+                    <div class="page-title-area">
+                        <div class="row align-items-center">
+                            <div class="col-sm-6">
+                                <div class="breadcrumbs-area clearfix">
+                                    <h4 class="page-title pull-left">My Leave History</h4>  
                                 </div>
+                            </div>
+                            <div class="col-sm-6 clearfix">
+                                <?php include '../includes/employee-profile-section.php'?>
                             </div>
                         </div>
                     </div>
-                    <!-- data table end -->
+                    <!-- page title area end -->
+                    <div class="main-content-inner">
+                        <div class="row">
+                            <!-- data table start -->
+                            <div class="col-12 mt-5">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="header-title">Leave History Table</h4>
+                                        <?php if($error){?><div class="alert alert-danger alert-dismissible fade show"><strong>Info: </strong><?php echo htmlentities($error); ?>
+                                        <button type="button"  class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    
+                                    </div><?php } 
+                                        else if($msg){?><div class="alert alert-success alert-dismissible fade show"><strong>Info: </strong><?php echo htmlentities($msg); ?> 
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        </div><?php }?>
+                                        <div class="data-tables">
+                                            <table id="dataTable" class="table table-hover progress-table text-center">
+                                                <thead class="bg-light text-capitalize">
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th width="150">Type</th>
+                                                        <th>Conditions</th>
+                                                        <th>From</th>
+                                                        <th>To</th>
+                                                        <th width="150">Applied</th>
+                                                        <th width="120">Admin's Remark</th>
+                                                        <th>Status</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                <?php 
+                                                $eid=$_SESSION['eid'];
+                                                $sql = "SELECT LeaveType,ToDate,FromDate,Description,PostingDate,AdminRemarkDate,AdminRemark,Status from tblleaves where empid=:eid";
+                                                $query = $dbh -> prepare($sql);
+                                                $query->bindParam(':eid',$eid,PDO::PARAM_STR);
+                                                $query->execute();
+                                                $results=$query->fetchAll(PDO::FETCH_OBJ);
+                                                $cnt=1;
+                                                if($query->rowCount() > 0){
+                                                foreach($results as $result)
+                                                {  ?> 
+
+                                                    <tr>
+                                                    <td> <?php echo htmlentities($cnt);?></td>
+                                                    <td><?php echo htmlentities($result->LeaveType);?></td>
+                                                    <td><?php echo htmlentities($result->Description);?></td>
+                                                    <td><?php echo htmlentities($result->FromDate);?></td>
+                                                    <td><?php echo htmlentities($result->ToDate);?></td>
+                                                    <td><?php echo htmlentities($result->PostingDate);?></td>
+                                                    <td><?php if($result->AdminRemark=="")
+                                                    {
+                                                    echo htmlentities('Pending');
+                                                    } else {
+
+                                                    echo htmlentities(($result->AdminRemark)." "."at"." ".$result->AdminRemarkDate);
+                                                    }
+
+                                                    ?>
+                                                    </td>
+
+                                                    <td> <?php $stats=$result->Status;
+                                                        if($stats==1){
+                                                    ?>
+                                                        <span style="color: green">Approved</span>
+                                                        <?php } if($stats==2)  { ?>
+
+                                                        <span style="color: red">Not Approved</span>
+                                                        <?php } if($stats==0)  { ?>
+
+                                                        <span style="color: blue">Pending</span>
+                                                        <?php } ?>
+
+                                                    </td>
+                                                </tr>
+
+                                                <?php $cnt++;} }?>
+                                                
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- data table end -->
+                        </div>
+                    </div>
                 </div>
+                <!-- main content area end -->
+                <!-- footer area start-->
+                <?php include '../includes/footer.php' ?>
+                <!-- footer area end-->
             </div>
+            <!-- page container area end -->
         </div>
-        <!-- main content area end -->
-        <!-- footer area start-->
-        <?php include '../includes/footer.php' ?>
-        <!-- footer area end-->
+
     </div>
-    <!-- page container area end -->
-    <!-- offset area start -->
-    <div class="offset-area">
-        <div class="offset-close"><i class="ti-close"></i></div>
-        
-        
-    </div>
-    <!-- offset area end -->
+
     <!-- jquery latest version -->
     <script src="../assets/js/vendor/jquery-2.2.4.min.js"></script>
     <!-- bootstrap 4 js -->
@@ -241,6 +245,8 @@
     <!-- others plugins -->
     <script src="../assets/js/plugins.js"></script>
     <script src="../assets/js/scripts.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../assets/js/adminlte.min.js"></script>
 </body>
 
 </html>
