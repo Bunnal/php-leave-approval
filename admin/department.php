07542c3cff1fc4c797997c4a0b3d9081e1782196
@Bunal <leangbunal432@gmail.com>
2022-03-06 14:54:12 +0700

    update link dashboard

--- admin/department.php
+++ admin/department.php
@@ -88,8 +88,8 @@
          <!-- Main Sidebar Container -->
          <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-            <img src="../assets/images/icon/eleave-logo2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <a href="dashboard.php" class="brand-link">
+            <img src="../assets/images/icon/eleave-logo2.png" alt="AdminLTE Logo" class="brand-image img-circle " style="opacity: .8">
             <span class="brand-text font-weight-light" style="margin-left: -20px;">Eleave</span>
             </a>
 
